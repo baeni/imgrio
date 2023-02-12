79571412696cbbdc500cf63a5f606d5710c3ceb4
baeni <baeni.saa@gmail.com>
2023-02-12 22:09:50 +0100

    Initial commit

---
+++ Dele.Blazor/Backend/Services/FirebaseAuthHandler.cs
@@ -0,0 +1,9 @@
+﻿using Firebase.Auth;
+
+namespace Dele.Blazor.Backend.Services
+{
+    public class FirebaseAuthHandler
+    {
+        public FirebaseAuthProvider FirebaseAuthProvider { get; } = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyAEIqYar3BalfukLcbscsuyrkoa-o3ie_c"));
+    }
+}
---
+++ Dele.Blazor/Program.cs
@@ -0,0 +1,54 @@
+using Dele.Blazor.Backend.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Dele.Blazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            builder.Services.AddAuthentication().AddCookie(options =>
+            {
+                options.LoginPath = "/u/sign-in";
+                options.LogoutPath = "/u/sign-out";
+            });
+            builder.Services.AddTransient<FirebaseAuthHandler>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseSession();
+            app.UseAuthorization();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
