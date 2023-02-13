1ec1dbf8c29561076a7533ac03067e5b802de8d6
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2023-02-13 20:11:39 +0100

    Create interface IUploadedFile

---
+++ Dele.Blazor/Backend/Interfaces/IUploadedFile.cs
@@ -0,0 +1,13 @@
+﻿namespace Fordele.Blazor.Backend.Interfaces
+{
+    public interface IUploadedFile
+    {
+        Guid Id { get; }
+
+        string Extension { get; }
+
+        DateTime CreatedAt { get; }
+
+        DateTime ModifiedAt { get; }
+    }
+}
