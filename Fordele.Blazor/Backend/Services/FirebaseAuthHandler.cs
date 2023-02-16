116f687e44a6428661d052aefeda44fc08893ca6
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2023-02-16 08:09:09 +0100

    Change web api key

--- Fordele.Blazor/Backend/Services/FirebaseAuthHandler.cs
+++ Fordele.Blazor/Backend/Services/FirebaseAuthHandler.cs
@@ -4,6 +4,6 @@ namespace Fordele.Blazor.Backend.Services
 {
     public class FirebaseAuthHandler
     {
-        public FirebaseAuthProvider FirebaseAuthProvider { get; } = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyAEIqYar3BalfukLcbscsuyrkoa-o3ie_c"));
+        public FirebaseAuthProvider FirebaseAuthProvider { get; } = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyAvgwBVchyVPYw44j6PpcfJl6W6bo4bTaE"));
     }
 }
