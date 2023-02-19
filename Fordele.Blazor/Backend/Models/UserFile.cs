cc9018efd22c07619afa0dff5661f354d26dc891
baeni <baeni.saa@gmail.com>
2023-02-19 23:06:12 +0100

    Remove UploadedDocument and UploadedImage models and replace by UserFile model

---
+++ Fordele.Blazor/Backend/Models/UserFile.cs
@@ -0,0 +1,33 @@
+﻿using Fordele.Blazor.Backend.Interfaces;
+
+namespace Fordele.Blazor.Backend.Models
+{
+    public class UserFile : IUserFile
+    {
+        public UserFile(Guid id, string title, string extension, double size, DateTime uploadedAt, string uploadedBy)
+        {
+            Id = id;
+            Title = title;
+            Extension = extension;
+            Size = size;
+            UploadedAt = uploadedAt;
+            UploadedBy = uploadedBy;
+        }
+
+        public Guid Id { get; }
+
+        public string Extension { get; }
+
+        public string FileName => $"{Id}.{Extension}";
+
+        public string FileUrl => Path.Combine("https://localhost:7223", "data", $"{Id}.{Extension}");
+
+        public string Title { get; }
+
+        public double Size { get; }
+
+        public DateTime UploadedAt { get; }
+
+        public string UploadedBy { get; }
+    }
+}
