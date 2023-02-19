251df86a8a98519fbe360f39ed1fdced75b10d98
baeni <baeni.saa@gmail.com>
2023-02-19 23:05:46 +0100

    Rename IUploadedFile to IUserFile; Add properties

---
+++ Fordele.Blazor/Backend/Interfaces/IUserFile.cs
@@ -0,0 +1,21 @@
+﻿namespace Fordele.Blazor.Backend.Interfaces
+{
+    public interface IUserFile
+    {
+        Guid Id { get; }
+
+        string Extension { get; }
+
+        string FileName { get; }
+
+        string FileUrl { get; }
+
+        string Title { get; }
+
+        double Size { get; }
+
+        DateTime UploadedAt { get; }
+
+        string UploadedBy { get; }
+    }
+}
