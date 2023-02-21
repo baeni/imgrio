fa97ba0af612ac3599c9652a90821d42a9234255
baeni <baeni.saa@gmail.com>
2023-02-21 19:29:48 +0100

    Remove outcommented code

--- Fordele.Blazor/Backend/Services/UserFileService.cs
+++ Fordele.Blazor/Backend/Services/UserFileService.cs
@@ -36,49 +36,5 @@ namespace Fordele.Blazor.Backend.Services
 
             return userFiles;
         }
-
-        //public IEnumerable<UploadedImage> GetUploadedImages()
-        //{
-        //    var uploadedImages = new List<UploadedImage>();
-
-        //    foreach (var imagePath in Directory.GetFiles(Path.Combine(DataPath, "images")))
-        //    {
-        //        var info = new FileInfo(imagePath);
-        //        var split = info.Name.Split('.');
-
-        //        var id = Guid.Parse(split[0]);
-        //        var extension = split[1];
-        //        var size = info.Length;
-        //        var createdAt = info.CreationTime;
-        //        var modifiedAt = info.LastWriteTime;
-
-        //        uploadedImages.Add(
-        //            new UploadedImage(id, extension, size, createdAt, modifiedAt));
-        //    }
-
-        //    return uploadedImages;
-        //}
-
-        //public IEnumerable<UserFile> GetUploadedDocuments()
-        //{
-        //    var uploadedDocuments = new List<UserFile>();
-
-        //    foreach (var documentPath in Directory.GetFiles(Path.Combine(DataPath, "documents")))
-        //    {
-        //        var info = new FileInfo(documentPath);
-        //        var split = info.Name.Split('.');
-
-        //        var id = Guid.Parse(split[0]);
-        //        var extension = split[1];
-        //        var size = info.Length;
-        //        var createdAt = info.CreationTime;
-        //        var modifiedAt = info.LastWriteTime;
-
-        //        uploadedDocuments.Add(
-        //            new UserFile(id, extension, size, createdAt, modifiedAt));
-        //    }
-
-        //    return uploadedDocuments;
-        //}
     }
 }
