2a3834a883b7f81df71e19823a14021ae497b979
baeni <baeni.saa@gmail.com>
2023-03-01 19:11:56 +0100

    Replace FirebaseAuthHandler and IHttpContextAccessor aith UserAuthService;

--- Imgrio.Blazor/Pages/User/SignInRedirect.cshtml.cs
+++ Imgrio.Blazor/Pages/User/SignInRedirect.cshtml.cs
@@ -10,77 +10,25 @@ namespace Imgrio.Blazor.Pages.User
 {
     public class SignInRedirectModel : PageModel
     {
-        private readonly FirebaseAuthHandler _firebaseAuthHandler;
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly UserAuthService _userAuthService;
 
-        public SignInRedirectModel(FirebaseAuthHandler firebaseAuthHandler, IHttpContextAccessor httpContextAccessor)
+        public SignInRedirectModel(UserAuthService userAuthService)
         {
-            _firebaseAuthHandler = firebaseAuthHandler;
-            _httpContextAccessor = httpContextAccessor;
+            _userAuthService = userAuthService;
         }
-
-        public string ReturnUrl { get; set; }
 
         public async Task<ActionResult> OnGetAsync(string email, string password)
         {
-            ReturnUrl = Url.Content("~/");
+            var user = await _userAuthService.SignInAsync(email, password);
 
-            try
+            if (user != null)
             {
-                var auth = await _firebaseAuthHandler.FirebaseAuthProvider.SignInWithEmailAndPasswordAsync(email, password);
-                var token = auth.FirebaseToken;
-
-                if (token == null)
-                {
-                    return LocalRedirect("/u/sign-in");
-                }
-
-                var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.NameIdentifier, auth.User.LocalId),
-                    new Claim(ClaimTypes.Name, auth.User.Email.Split('@')[0]),
-                    new Claim(ClaimTypes.Email, auth.User.Email),
-                    new Claim(ClaimTypes.Role, "User"),
-                };
-
-                var claimsIdentity = new ClaimsIdentity(
-                    claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-                var authProperties = new AuthenticationProperties
-                {
-                    AllowRefresh = true,
-                    // Refreshing the authentication session should be allowed.
-
-                    //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-                    // The time at which the authentication ticket expires. A 
-                    // value set here overrides the ExpireTimeSpan option of 
-                    // CookieAuthenticationOptions set with AddCookie.
-
-                    IsPersistent = true,
-                    // Whether the authentication session is persisted across 
-                    // multiple requests. When used with cookies, controls
-                    // whether the cookie's lifetime is absolute (matching the
-                    // lifetime of the authentication ticket) or session-based.
-
-                    //IssuedUtc = <DateTimeOffset>,
-                    // The time at which the authentication ticket was issued.
-
-                    //RedirectUri = <string>
-                    // The full path or absolute URI to be used as an http 
-                    // redirect response value.
-                };
-
-                await _httpContextAccessor.HttpContext.SignInAsync(
-                    CookieAuthenticationDefaults.AuthenticationScheme,
-                    new ClaimsPrincipal(claimsIdentity),
-                    authProperties);
+                return LocalRedirect("/u/files");
             }
-            catch (FirebaseAuthException)
+            else
             {
                 return LocalRedirect("/u/sign-in");
             }
-
-            return LocalRedirect("/u/files");
         }
     }
 }
