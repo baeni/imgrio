abf905823d96d6a78205ed4277bd7ed50053b6ac
baeni <baeni.saa@gmail.com>
2023-03-02 22:28:38 +0100

    Add nullchecks and remove FirebaseUser?.DisplayName as this property is always an empty string

--- Imgrio.Blazor/Backend/Models/UserState.cs
+++ Imgrio.Blazor/Backend/Models/UserState.cs
@@ -15,14 +15,14 @@ namespace Imgrio.Blazor.Backend.Models
 
         public User? FirebaseUser { get; set; }
 
-        public bool IsAuthenticated => FirebaseUser != null && _httpContextAccessor?.HttpContext?.User?.Identity?.IsAuthenticated == true;
+        public bool IsAuthenticated => FirebaseUser != null && _httpContextAccessor.HttpContext?.User?.Identity?.IsAuthenticated == true;
 
-        public string Id => FirebaseUser?.LocalId ?? _httpContextAccessor?.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+        public string Id => FirebaseUser?.LocalId ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
 
-        public string Email => FirebaseUser?.Email ?? _httpContextAccessor?.HttpContext?.User?.FindFirstValue(ClaimTypes.Email);
+        public string Email => FirebaseUser?.Email ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Email);
 
-        public string Name => FirebaseUser?.DisplayName ?? _httpContextAccessor?.HttpContext?.User?.FindFirstValue(ClaimTypes.Name);
+        public string Name => /*FirebaseUser?.DisplayName ?? */_httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Name);
 
-        public string Role => _httpContextAccessor?.HttpContext?.User?.FindFirstValue(ClaimTypes.Role);
+        public string Role => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Role);
     }
 }
