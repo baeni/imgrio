7996603a3d56bae1d29dd553127cc2db00e815ca
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2023-03-02 17:09:35 +0100

    Create implementation of IUserState

---
+++ Imgrio.Blazor/Backend/Models/UserState.cs
@@ -0,0 +1,28 @@
+﻿using Firebase.Auth;
+using Imgrio.Blazor.Backend.Interfaces;
+using System.Security.Claims;
+
+namespace Imgrio.Blazor.Backend.Models
+{
+    public class UserState : IUserState
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserState(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public User? FirebaseUser { get; set; }
+
+        public bool IsAuthenticated => FirebaseUser != null && _httpContextAccessor?.HttpContext?.User?.Identity?.IsAuthenticated == true;
+
+        public string Id => FirebaseUser?.LocalId ?? _httpContextAccessor?.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        public string Email => FirebaseUser?.Email ?? _httpContextAccessor?.HttpContext?.User?.FindFirstValue(ClaimTypes.Email);
+
+        public string Name => FirebaseUser?.DisplayName ?? _httpContextAccessor?.HttpContext?.User?.FindFirstValue(ClaimTypes.Name);
+
+        public string Role => _httpContextAccessor?.HttpContext?.User?.FindFirstValue(ClaimTypes.Role);
+    }
+}
