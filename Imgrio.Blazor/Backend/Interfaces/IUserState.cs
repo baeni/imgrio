177258eb0c8c92c1dcb61ef170a8d72cd72730b7
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2023-03-02 17:09:28 +0100

    Create IUserState

---
+++ Imgrio.Blazor/Backend/Interfaces/IUserState.cs
@@ -0,0 +1,20 @@
+﻿using Firebase.Auth;
+
+namespace Imgrio.Blazor.Backend.Interfaces
+{
+    public interface IUserState
+    {
+        User? FirebaseUser { get; set; }
+
+        bool IsAuthenticated { get; }
+        
+        string Id { get; }
+        
+        string Email { get; }
+        
+        string Name { get; }
+        
+        string Role { get; }
+
+    }
+}
