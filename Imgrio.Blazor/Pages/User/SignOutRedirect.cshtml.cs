ea8cbf5e0541c773dccc95efda1a1748fd9fbd06
bennsaa <baeni.saa@gmail.com>
2023-03-03 14:13:20 +0100

    Temporarily outcomment SignOutRedirect.cshtml and .cshtml.cs

--- Imgrio.Blazor/Pages/User/SignOutRedirect.cshtml.cs
+++ Imgrio.Blazor/Pages/User/SignOutRedirect.cshtml.cs
@@ -1,23 +1,23 @@
-using Imgrio.Blazor.Backend.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Imgrio.Blazor.Backend.Services;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Imgrio.Blazor.Pages.User
-{
-    public class SignOutRedirectModel : PageModel
-    {
-        private readonly UserAuthService _userAuthService;
+//namespace Imgrio.Blazor.Pages.User
+//{
+//    public class SignOutRedirectModel : PageModel
+//    {
+//        private readonly UserAuthService _userAuthService;
 
-        public SignOutRedirectModel(UserAuthService userAuthService)
-        {
-            _userAuthService = userAuthService;
-        }
+//        public SignOutRedirectModel(UserAuthService userAuthService)
+//        {
+//            _userAuthService = userAuthService;
+//        }
 
-        public async Task<ActionResult> OnGetAsync()
-        {
-            await _userAuthService.SignOutAsync();
+//        public async Task<ActionResult> OnGetAsync()
+//        {
+//            await _userAuthService.SignOutAsync();
 
-            return LocalRedirect("/");
-        }
-    }
-}
+//            return LocalRedirect("/");
+//        }
+//    }
+//}
