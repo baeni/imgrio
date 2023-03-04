f330001e82f4aabd16a86cbe71aa8671292657c2
baeni <baeni.saa@gmail.com>
2023-03-04 20:55:51 +0100

    Create

---
+++ Imgrio.Blazor/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Imgrio.Blazor.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+    }
+}
