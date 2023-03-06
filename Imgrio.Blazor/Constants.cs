15310b9c8e35eeafdf5740afc72e69aa84392e33
baeni <baeni.saa@gmail.com>
2023-03-06 21:44:29 +0100

    Create static Constants class

---
+++ Imgrio.Blazor/Constants.cs
@@ -0,0 +1,20 @@
+﻿namespace Imgrio.Blazor
+{
+    public static class Constants
+    {
+        public const string PathToIndex = "/";
+        public const string PathToSharex = "/sharex";
+        public const string PathToImprint = "/imprint";
+        public const string PathToPrivacyPolicy = "/privacy-policy";
+        public const string PathToCopyright = "https://baeni.de";
+        public const string PathToAuthRegister = "/a/sign-up";
+        public const string PathToAuthLogin = "/a/sign-in";
+        public const string PathToAuthLogout = "/a/sign-out";
+        public const string PathToFileUpload = "/f/upload";
+        public const string PathToFileView = "/f";
+        public const string PathToUserAccount = "/u/account";
+        public const string PathToUserFiles = "/u/files";
+
+        public const int MaxFileSizeInBytes = 10000000;
+    }
+}
