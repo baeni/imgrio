ce803b675e876932208fc5d9db55f66001114820
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2023-03-07 08:50:24 +0100

    Add constant AppName; Adjust path for constants PathToAuthRegister -Login and -Logout

--- Imgrio.Blazor/Constants.cs
+++ Imgrio.Blazor/Constants.cs
@@ -2,14 +2,16 @@
 {
     public static class Constants
     {
+        public const string AppName = "imgrio";
+
         public const string PathToIndex = "/";
         public const string PathToSharex = "/sharex";
         public const string PathToImprint = "/imprint";
         public const string PathToPrivacyPolicy = "/privacy-policy";
         public const string PathToCopyright = "https://baeni.de";
-        public const string PathToAuthRegister = "/a/sign-up";
-        public const string PathToAuthLogin = "/a/sign-in";
-        public const string PathToAuthLogout = "/a/sign-out";
+        public const string PathToAuthRegister = "/a/register";
+        public const string PathToAuthLogin = "/a/login";
+        public const string PathToAuthLogout = "/a/logout";
         public const string PathToFileUpload = "/f/upload";
         public const string PathToFileView = "/f";
         public const string PathToUserAccount = "/u/account";
