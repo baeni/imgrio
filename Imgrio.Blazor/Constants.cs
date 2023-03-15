0c764b29d849617a7bb35452e258fa5087346e75
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2023-03-15 09:16:47 +0100

    Re-order constants; Make routes more explicit; Add constants for symbols

--- Imgrio.Blazor/Constants.cs
+++ Imgrio.Blazor/Constants.cs
@@ -7,18 +7,27 @@
                                                     ? "https://imgrio.azurewebsites.net"
                                                     : "https://localhost:7223";
 
+        public const string SymbolAppLogo = "/assets/symbols/logo.svg";
+        public const string SymbolFiles = "/assets/symbols/files.svg";
+        public const string SymbolUpload = "/assets/symbols/upload.svg";
+        public const string SymbolAccount = "/assets/symbols/account.svg";
+        public const string SymbolLogout = "/assets/symbols/logout.svg";
+
         public const string PathToIndex = "/";
         public const string PathToSharex = "/sharex";
+        public const string PathToView = "/f/";
+
         public const string PathToImprint = "/imprint";
         public const string PathToPrivacyPolicy = "/privacy-policy";
         public const string PathToCopyright = "https://baeni.de";
-        public const string PathToAuthRegister = "/a/register";
-        public const string PathToAuthLogin = "/a/login";
-        public const string PathToAuthLogout = "/a/logout";
-        public const string PathToFileUpload = "/f/upload";
-        public const string PathToFileView = "/f/";
-        public const string PathToUserAccount = "/u/account";
-        public const string PathToUserFiles = "/u/files";
+
+        public const string PathToAuthRegister = "/account/register";
+        public const string PathToAuthLogin = "/account/login";
+        public const string PathToAuthLogout = "/account/logout";
+
+        public const string PathToDashboardUpload = "/dashboard/upload";
+        public const string PathToDashboardAccount = "/dashboard/account";
+        public const string PathToDashboardFiles = "/dashboard/files";
 
         public const int MaxFileSizeInBytes = 10000000;
     }
