f55d9747c58cd81ede754b9cee3732df37bb1dc6
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2023-07-08 00:36:57 +0200

    Create UsersController

---
+++ imgrio-api/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using imgrio_api.Data;
+using imgrio_api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace imgrio_api.Controllers
+{
+    /*
+     * Endpoint to managa files:
+     * - Get information about all users
+     */
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ImgrioDbContext _dbContext;
+
+        public UsersController(ImgrioDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsersInfo()
+        {
+            var set = _dbContext.Set<UploadedFile>();
+
+            var count = await set.Select(x => x.UploadedBy).Distinct().CountAsync();
+            var countToday = await set
+                .Where(x => x.UploadedAt.Day == DateTime.UtcNow.Day)
+                .Select(x => x.UploadedBy)
+                .Distinct()
+                .CountAsync();
+
+            return Ok(new
+            {
+                count,
+                countToday
+            });
+        }
+    }
+}
