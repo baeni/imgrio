73eef7c1e3d5aceb87941ea119a553f935432d9b
baeni <baeni.saa@gmail.com>
2023-07-09 12:16:19 +0200

    Implement checkAuthentication, handleLogin and handleLogout functions

--- imgrio-react/src/components/navbar/Navbar.jsx
+++ imgrio-react/src/components/navbar/Navbar.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './navbar.css';
 
+import { useMsal } from '@azure/msal-react';
+import { loginRequest } from '../../authConfig';
+
 const Navbar = () => {
+  const { instance } = useMsal();
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        console.info('test');
+        const response = await instance.acquireTokenSilent(loginRequest);
+        console.info(response.accessToken);
+        setAuthenticated(true);
+      } catch (error) {
+        setAuthenticated(false);
+      }
+    };
+
+    checkAuthentication();
+  }, [instance]);
+
+  const handleLogin = async () => {
+    try {
+      await instance.loginPopup(loginRequest);
+    } catch (error) {
+      console.error('Login error:', error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await instance.logoutRedirect();
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
+  };
+
   return (
     <div className="navbar">
       <div className="navbar__links">
@@ -14,7 +51,11 @@ const Navbar = () => {
         </div>
       </div>
       <div className="navbar__sign">
-        <button className="btn">Anmelden</button>
+        {
+          authenticated
+          ? (<button className="btn" onClick={handleLogout}>Abmelden</button>)
+          : (<button className="btn" onClick={handleLogin}>Anmelden</button>)
+        }
       </div>
     </div>
   )
