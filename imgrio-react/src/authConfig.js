3587103daefff32ccbc561e3c11afc798e6457ef
baeni <baeni.saa@gmail.com>
2023-07-09 22:34:11 +0200

    Add postLogoutRedirectUri

--- imgrio-react/src/authConfig.js
+++ imgrio-react/src/authConfig.js
@@ -5,8 +5,9 @@ const msalConfig = {
     clientId: 'd9bf5b2c-ee47-40fd-8b61-cc6cd86d2582',
     authority: 'https://login.microsoftonline.com/870c73fe-c22b-46ff-87c3-b7bf53ed6125',
     redirectUri: 'http://localhost:3000',
+    postLogoutRedirectUri: 'http://localhost:3000',
     responseType: 'code',
-    navigateToLoginRequestUrl: false
+    navigateToLoginRequestUrl: false,
   }
 };
 
