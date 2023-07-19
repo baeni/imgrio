84e2d0751fa714ac3598f9235c8ffd4e240dc9af
baeni <baeni.saa@gmail.com>
2023-07-19 22:22:58 +0200

    Configure vue-toastification

--- imgrio-vue/src/main.ts
+++ imgrio-vue/src/main.ts
@@ -1,4 +1,5 @@
 import './assets/main.css';
+import 'vue-toastification/dist/index.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
@@ -7,11 +8,20 @@ import App from './App.vue';
 import router from './router';
 import { msalPlugin } from './plugins/msalPlugin';
 import { msalInstance } from './authConfig';
+import Toast from 'vue-toastification';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(msalPlugin, msalInstance);
+app.use(Toast, {
+  transition: 'Vue-Toastification__fade',
+  position: 'top-center',
+  hideProgressBar: true,
+  showCloseButtonOnHover: true,
+  maxToasts: 1,
+  timeout: 2000
+});
 
 app.mount('#app');
