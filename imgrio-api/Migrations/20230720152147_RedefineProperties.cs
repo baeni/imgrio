f5abe7b05da080b55c44c9e14fc59c28655c2a5e
baeni <baeni.saa@gmail.com>
2023-07-20 17:27:54 +0200

    Add migration "RedefineProperties"

---
+++ imgrio-api/Migrations/20230720152147_RedefineProperties.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace imgrio_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RedefineProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "UploadedFiles",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "IsExternal",
+                table: "UploadedFiles",
+                newName: "IsSelfHosted");
+
+            migrationBuilder.RenameColumn(
+                name: "ExternalUri",
+                table: "UploadedFiles",
+                newName: "Url");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "UploadedFiles",
+                newName: "ExternalUri");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "UploadedFiles",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "IsSelfHosted",
+                table: "UploadedFiles",
+                newName: "IsExternal");
+        }
+    }
+}
