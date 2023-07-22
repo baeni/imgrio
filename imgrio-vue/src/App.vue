b83649bd9bed1d6830f1e1d01722421ff686e22d
baeni <baeni.saa@gmail.com>
2023-07-22 17:39:31 +0200

    Implement logic to fetch UserDetails on login

--- imgrio-vue/src/App.vue
+++ imgrio-vue/src/App.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+
+import { useUserDetailsStore } from './stores/userDetails';
+import { useMsal } from './composition-api/useMsal';
+import { onMounted } from 'vue';
+import { watch } from 'vue';
+
+const userDetailsStore = useUserDetailsStore();
+const msal = useMsal();
+const accounts = msal.accounts.value;
+
+onMounted(() => {
+  if (accounts.length !== 0) {
+    userDetailsStore.fetchUserDetails();
+  }
+});
+
+watch(accounts, () => {
+  if (accounts.length === 0) {
+    userDetailsStore.userDetails = null;
+    return;
+  }
+
+  userDetailsStore.fetchUserDetails();
+});
 </script>
 
 <template>
