8720751570f67d26b9eebcbb1eb2d11bc290ff01
baeni <baeni.saa@gmail.com>
2023-07-22 18:21:26 +0200

    Set baseURL for axios to production api URL

--- imgrio-vue/src/axios.ts
+++ imgrio-vue/src/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { msalInstance, loginRequest } from './authConfig';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'http://api.imgrio.com'
 });
 
 apiClient.interceptors.request.use(async (config) => {
