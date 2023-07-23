c102946abd6a6192514caa9d7b9cdcf7d15301e0
baeni <baeni.saa@gmail.com>
2023-07-23 22:50:47 +0200

    Adjust subtitle

--- imgrio-vue/src/views/dashboard/FilesView.vue
+++ imgrio-vue/src/views/dashboard/FilesView.vue
@@ -16,7 +16,7 @@ const userFiles = computed(() => userFilesStore.userFiles);
     </div>
     <div v-if="userFiles">
       <div class="section__container-subtitle">
-        <p>{{ userFiles?.length }}</p>
+        <p>Du teilst aktuell {{ userFiles?.length }} Dateien mit Imgrio</p>
       </div>
       <div class="section__container-list">
         <FileCard :file="file" v-for="file in userFiles" :key="file.id" />
