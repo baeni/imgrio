9df1ec6b002342d2bc23f1d9c0cdb8aa901b786e
baeni <baeni.saa@gmail.com>
2023-07-24 19:19:30 +0200

    Remove redunant class

--- imgrio-vue/src/views/ShareXView.vue
+++ imgrio-vue/src/views/ShareXView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="section__container section--margin">
-    <div class="section__container-title section__title">
+    <div class="section__title">
       <h1>ShareX</h1>
     </div>
   </div>
@@ -9,11 +9,5 @@
 <style scoped>
 .section__container {
   color: #fff;
-}
-
-.section__container-title {
-  font-family: var(--font-family);
-  font-size: 1rem;
-  line-height: 55px;
 }
 </style>
