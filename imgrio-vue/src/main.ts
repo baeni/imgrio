6d841c626e58889465d669fb91e313704576a5c6
baeni <baeni.saa@gmail.com>
2023-07-26 22:21:24 +0200

    Clean up

--- imgrio-vue/src/main.ts
+++ imgrio-vue/src/main.ts
@@ -1,20 +1,22 @@
 import './assets/main.css';
-import 'vue-toastification/dist/index.css';
 
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-
 import App from './App.vue';
 import router from './router';
+
 import { msalPlugin } from './plugins/msalPlugin';
 import { msalInstance } from './authConfig';
+
+import { createPinia } from 'pinia';
+
+import 'vue-toastification/dist/index.css';
 import Toast from 'vue-toastification';
 
 const app = createApp(App);
 
-app.use(createPinia());
 app.use(router);
 app.use(msalPlugin, msalInstance);
+app.use(createPinia());
 app.use(Toast, {
   transition: 'Vue-Toastification__fade',
   position: 'top-center',
