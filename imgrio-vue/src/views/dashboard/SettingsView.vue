9e25a169628bd1b0e7d5ca3656cf769f47c49880
baeni <baeni.saa@gmail.com>
2023-07-31 22:16:52 +0200

    Add Access Token input-group

--- imgrio-vue/src/views/dashboard/SettingsView.vue
+++ imgrio-vue/src/views/dashboard/SettingsView.vue
@@ -43,6 +43,17 @@
         </select>
       </div>
 
+      <div class="section__container-form-input-group">
+        <label for="firstName">Access Token</label>
+        <div>
+          <Knob
+            text="Neuen Token generieren"
+            small
+            @click="() => toast.success('Neuer Token in Zwischenablage kopiert')"
+          />
+        </div>
+      </div>
+
       <div class="section__container-form-button">
         <Knob
           text="Änderungen speichern"
@@ -62,7 +73,7 @@ import { useUserDetailsStore } from '@/stores/userDetails';
 import type { User } from '@microsoft/microsoft-graph-types';
 import { useToast } from 'vue-toastification';
 
-import Knob from '@/components/Knob.vue';
+import Knob from '@/components/inputs/Knob.vue';
 
 const toast = useToast();
 
@@ -101,15 +112,27 @@ const userDetails = computed(() => userDetailsStore.userDetails) as User;
 .section__container-form-input-group input,
 .section__container-form-input-group select {
   flex: 2.75;
-  padding: 0.5rem 1rem;
+  padding: 0.75rem 1rem;
+  box-sizing: border-box;
   font-family: var(--font-family);
   font-size: 0.9rem;
-  line-height: 30px;
   background: var(--color-dark);
   color: var(--color-light);
   border: none;
   border-radius: 10px;
   outline: none;
+}
+
+.section__container-form-input-group select {
+  appearance: none;
+  background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='3' stroke-linecap='round' stroke-linejoin='round'%3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e");
+  background-repeat: no-repeat;
+  background-position: right 1rem center;
+  background-size: 1rem;
+}
+
+.section__container-form-input-group div {
+  flex: 2.75;
 }
 
 .section__container-form-input-group input:disabled,
