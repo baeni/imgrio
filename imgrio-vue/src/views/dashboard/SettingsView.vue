abc8bfccfeee5174b53121876d9d01bf65d25a80
baeni <baeni.saa@gmail.com>
2023-08-04 01:04:25 +0200

    Implement inputs components

--- imgrio-vue/src/views/dashboard/SettingsView.vue
+++ imgrio-vue/src/views/dashboard/SettingsView.vue
@@ -6,52 +6,59 @@
     <form class="section__container-form" v-if="userDetails">
       <div class="section__container-form-input-group">
         <label for="firstName">Vorname</label>
-        <input type="text" id="firstName" :value="userDetails.givenName" disabled />
+        <TextField id="firstName" :value="userDetails.givenName" disabled />
       </div>
 
       <div class="section__container-form-input-group">
         <label for="lastName">Nachname</label>
-        <input type="text" id="lastName" :value="userDetails.surname" disabled />
+        <TextField id="lastName" :value="userDetails.surname" disabled />
       </div>
 
       <div class="section__container-form-input-group">
         <label for="email">Email</label>
-        <input type="email" id="email" :value="userDetails.mail" disabled />
+        <TextField id="email" :value="userDetails.mail" type="email" disabled />
       </div>
 
       <div class="section__container-form-input-group">
         <label for="imageAnimation">Bild-Animation</label>
-        <select id="imageAnimation">
-          <option value="false">Inaktiv</option>
-          <option value="true">Aktiv</option>
-        </select>
+        <Dropdown
+          id="imageAnimation"
+          :options="[
+            { key: 'Inaktiv', value: false },
+            { key: 'Aktiv', value: true }
+          ]"
+        />
       </div>
 
       <div class="section__container-form-input-group">
         <label for="externalHost">Datei-Server</label>
-        <select id="externalHost">
-          <option value="false">imgrio</option>
-          <option value="true">Eigener</option>
-        </select>
+        <Dropdown
+          id="externalHost"
+          :options="[
+            { key: 'imgrio', value: false },
+            { key: 'Eeigener', value: true }
+          ]"
+        />
       </div>
 
       <div class="section__container-form-input-group">
         <label for="externalHost">Sprache</label>
-        <select id="externalHost">
-          <option value="de-de">Deutsch</option>
-          <option value="en-us">Englisch</option>
-        </select>
+        <Dropdown
+          id="language"
+          :options="[
+            { key: 'Deutsch', value: 'de' },
+            { key: 'English', value: 'en' }
+          ]"
+        />
       </div>
 
       <div class="section__container-form-input-group">
         <label for="firstName">Access Token</label>
-        <div>
-          <Knob
-            text="Neuen Token generieren"
-            small
-            @click="() => toast.success('Neuer Token in Zwischenablage kopiert')"
-          />
-        </div>
+        <Knob
+          text="Neu generieren"
+          small
+          @click="() => toast.success('Neuer Token in Zwischenablage kopiert')"
+        />
       </div>
 
       <div class="section__container-form-button">
@@ -73,6 +80,8 @@ import { useUserDetailsStore } from '@/stores/userDetails';
 import type { User } from '@microsoft/microsoft-graph-types';
 import { useToast } from 'vue-toastification';
 
+import TextField from '@/components/inputs/TextField.vue';
+import Dropdown from '@/components/inputs/Dropdown.vue';
 import Knob from '@/components/inputs/Knob.vue';
 
 const toast = useToast();
@@ -110,39 +119,10 @@ const userDetails = computed(() => userDetailsStore.userDetails) as User;
 }
 
 .section__container-form-input-group input,
-.section__container-form-input-group select {
+.section__container-form-input-group select,
+.section__container-form-input-group submit {
   flex: 2.75;
-  padding: 0.75rem 1rem;
-  box-sizing: border-box;
-  font-family: var(--font-family);
-  font-size: 0.9rem;
-  background: var(--color-dark);
-  color: var(--color-light);
-  border: none;
-  border-radius: 10px;
-  outline: none;
-}
-
-.section__container-form-input-group select {
-  appearance: none;
-  background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='3' stroke-linecap='round' stroke-linejoin='round'%3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e");
-  background-repeat: no-repeat;
-  background-position: right 1rem center;
-  background-size: 1rem;
-}
-
-.section__container-form-input-group div {
-  flex: 2.75;
-}
-
-.section__container-form-input-group input:disabled,
-.section__container-form-input-group select:disabled {
-  filter: brightness(0.5);
-  cursor: not-allowed;
-}
-
-.section__container-form-input-group select {
-  cursor: pointer;
+  margin: 0;
 }
 
 .section__container-form-button {
