ea91fca43b057b9ba013f5cb5789f9f9ebbd5848
baeni <baeni.saa@gmail.com>
2023-08-09 19:34:43 +0200

    Remove router transitions

--- imgrio-vue/src/layouts/DashboardLayout.vue
+++ imgrio-vue/src/layouts/DashboardLayout.vue
@@ -2,18 +2,7 @@
   <NavBar style="background: var(--color-darkest); height: 4rem" />
   <SubNavBar />
 
-  <router-view v-slot="{ Component, route }">
-    <transition
-      enter-from-class="opacity-0"
-      enter-to-class="opacity-100"
-      enter-active-class="transition-opacity duration-500"
-      leave-from-class="opacity-100"
-      leave-to-class="opacity-0"
-      leave-active-class="transition-opacity duration-500"
-    >
-      <component :is="Component" :key="route.path" />
-    </transition>
-  </router-view>
+  <RouterView />
 </template>
 
 <script setup lang="ts">
--- imgrio-vue/src/layouts/DefaultLayout.vue
+++ imgrio-vue/src/layouts/DefaultLayout.vue
@@ -1,18 +1,7 @@
 <template>
   <NavBar />
 
-  <router-view v-slot="{ Component, route }">
-    <transition
-      enter-from-class="opacity-0"
-      enter-to-class="opacity-100"
-      enter-active-class="transition-opacity duration-500"
-      leave-from-class="opacity-100"
-      leave-to-class="opacity-0"
-      leave-active-class="transition-opacity duration-500"
-    >
-      <component :is="Component" :key="route.path" />
-    </transition>
-  </router-view>
+  <RouterView />
 </template>
 
 <script setup lang="ts">
