0760d57681ad14fb5a93d8105963d4fd642a0623
baeni <baeni.saa@gmail.com>
2023-08-09 21:23:20 +0200

    Change arrow icon

--- imgrio-vue/src/components/FileCard.vue
+++ imgrio-vue/src/components/FileCard.vue
@@ -10,10 +10,10 @@
             </p>
           </div>
           <div class="card__container-info-icon">
-            <svg viewBox="0 0 28 25" class="icon" fill="#fff">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512" fill="#fff">
               <path
-                d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"
-              ></path>
+                d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
+              />
             </svg>
           </div>
         </div>
@@ -105,14 +105,8 @@ function playAnimation() {
 }
 
 @keyframes jump {
-  0% {
-    transform: scale(1);
-  }
   50% {
-    transform: translateY(-0.25rem);
-  }
-  100% {
-    transform: scale(1);
+    transform: translateY(-2%);
   }
 }
 </style>
