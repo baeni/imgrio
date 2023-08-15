bfe0a98eaafdbca14e4e6175b2af12f409749d5b
baeni <baeni.saa@gmail.com>
2023-08-15 17:55:00 +0200

    Add Bearer for authorization and set GetAllUsersInfo to AllowAnonymous; Add GetPermanentJwt handler

--- imgrio-api/Controllers/UsersController.cs
+++ imgrio-api/Controllers/UsersController.cs
@@ -1,7 +1,12 @@
 ﻿using imgrio_api.Data;
 using imgrio_api.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace imgrio_api.Controllers
 {
@@ -12,16 +17,19 @@ namespace imgrio_api.Controllers
 
     [ApiController]
     [Route("[controller]")]
+    [Authorize(AuthenticationSchemes = "Bearer")]
     public class UsersController : ControllerBase
     {
         private readonly ImgrioDbContext _dbContext;
+        private readonly IConfiguration _configuration;
 
-        public UsersController(ImgrioDbContext dbContext)
+        public UsersController(ImgrioDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _configuration = configuration;
         }
 
-        [HttpGet]
+        [HttpGet, AllowAnonymous]
         public async Task<IActionResult> GetAllUsersInfo()
         {
             var set = _dbContext.Set<UserFile>();
@@ -38,6 +46,36 @@ namespace imgrio_api.Controllers
                 count,
                 countToday
             });
+        }
+
+        [HttpGet("{userId}")]
+        public IActionResult GetPermanentJwt(Guid userId)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>()
+            {
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new(JwtRegisteredClaimNames.Sub, userId.ToString())
+            };
+
+            var dateNow = DateTime.UtcNow;
+
+            var token = new JwtSecurityToken(
+                audience: _configuration["Jwt:Audience"],
+                issuer: _configuration["Jwt:Issuer"],
+                notBefore: dateNow,
+                expires: dateNow.AddSeconds(30),
+                claims: claims,
+
+                signingCredentials: credentials
+            );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwt = tokenHandler.WriteToken(token);
+
+            return Ok(jwt);
         }
     }
 }
