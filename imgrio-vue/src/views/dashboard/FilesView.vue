f29d09f335c7ffe2e27776d7fabc511aa232c656
baeni <baeni.saa@gmail.com>
2023-08-15 22:11:03 +0200

    Add Knob component to Favorites in div for section title; Remove subtitle;

--- imgrio-vue/src/views/dashboard/FilesView.vue
+++ imgrio-vue/src/views/dashboard/FilesView.vue
@@ -2,6 +2,7 @@
   <div class="section__container section--margin">
     <div class="section__title">
       <h1>Meine Dateien</h1>
+      <Knob text="Favoriten" href="./favorites" small />
     </div>
 
     <div class="section__container-status grid place-items-center" v-if="!fetched">
@@ -13,9 +14,6 @@
     </div>
 
     <div v-else>
-      <div class="section__container-subtitle">
-        <p>Du teilst aktuell {{ userFiles?.length }} Dateien mit imgrio</p>
-      </div>
       <div class="section__container-list">
         <FileCard :userFile="file" v-for="file in userFiles" :key="file.id" />
       </div>
@@ -27,6 +25,7 @@
 import { computed, ref, reactive, onMounted } from 'vue';
 import { useUserFilesStore } from '@/stores/userFiles';
 
+import Knob from '@/components/inputs/Knob.vue';
 import Loading from '../../components/Loading.vue';
 import FileCard from '../../components/FileCard.vue';
 import type { UserFile } from '@/models';
@@ -64,12 +63,9 @@ const userFiles = computed(() => userFilesData.userFiles);
   color: #fff;
 }
 
-.section__container-subtitle {
-  padding-bottom: 1rem;
-  color: var(--color-title);
-  font-family: var(--font-family);
-  font-size: 0.9rem;
-  line-height: 25px;
+.section__title {
+  display: flex;
+  justify-content: space-between;
 }
 
 .section__container-list {
