7e1eb1701a5e915c0e6e212d5b57abd86ec057f2
baeni <baeni.saa@gmail.com>
2023-08-27 14:43:23 +0200

    Remove obsolete error handline

--- imgrio-nuxt/axios.conf.ts
+++ imgrio-nuxt/axios.conf.ts
@@ -5,26 +5,23 @@ const apiClient = axios.create({
   baseURL: "https://api.imgrio.com",
 });
 
-apiClient.interceptors.request.use(
-  async (config) => {
-    const currentUser = getAuth().currentUser;
+apiClient.interceptors.request.use(async (config) => {
+  const currentUser = getAuth().currentUser;
 
-    if (!currentUser) {
-      console.log("Nobody");
-      return config;
-    }
+  if (!currentUser) {
+    console.log("Nobody");
+    return config;
+  }
 
-    try {
-      const token = await currentUser.getIdToken();
+  try {
+    const token = await currentUser.getIdToken();
 
-      config.headers.Authorization = `Bearer ${token}`;
-      return config;
-    } catch (error) {
-      console.error(error);
-      return Promise.reject(error);
-    }
-  },
-  (error) => Promise.reject(error)
-);
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  } catch (error) {
+    console.error(error);
+    return Promise.reject(error);
+  }
+});
 
 export { apiClient };
