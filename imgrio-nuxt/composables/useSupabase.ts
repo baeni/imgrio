d2233512dca5746a69c7f3bd4925af09d4223df9
baeni <baeni.saa@gmail.com>
2023-08-27 20:54:05 +0200

    Add useSupabase composable

---
+++ imgrio-nuxt/composables/useSupabase.ts
@@ -0,0 +1,24 @@
+export const signInUser = async () => {
+  const supabase = useSupabaseClient();
+
+  try {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        queryParams: {
+          access_type: "offline",
+          prompt: "select_account",
+        },
+      },
+    });
+    if (error) throw error;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signOutUser = async () => {
+  const supabase = useSupabaseClient();
+
+  const { error } = await supabase.auth.signOut();
+};
