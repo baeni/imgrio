27af9459f1627b631fd037e8c970b268ebce04eb
baeni <baeni.saa@gmail.com>
2023-08-27 20:57:06 +0200

    Remove firebase configurations; Add supabase configurations

--- imgrio-nuxt/nuxt.config.ts
+++ imgrio-nuxt/nuxt.config.ts
@@ -10,26 +10,11 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ["@pinia/nuxt", "nuxt-vuefire", "@nuxtjs/i18n"],
+  modules: ["@pinia/nuxt", "@nuxtjs/i18n", "@nuxtjs/supabase"],
   build: { transpile: ["vue-toastification"] },
 
   runtimeConfig: {
-    public: {
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-    },
-  },
-
-  vuefire: {
-    config: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: "imgrio-b1ddc.firebaseapp.com",
-      projectId: "imgrio-b1ddc",
-      appId: "1:469817125060:web:81ca004d19dd661e0e4191",
-      storageBucket: "imgrio-b1ddc.appspot.com",
-      messagingSenderId: "469817125060",
-      measurementId: "G-WNEXEH32BG",
-    },
-    auth: true,
+    public: {},
   },
 
   i18n: {
@@ -42,5 +27,13 @@ export default defineNuxtConfig({
     ],
     langDir: "./locales",
     strategy: "no_prefix",
+  },
+
+  supabase: {
+    redirectOptions: {
+      login: "/",
+      callback: "/",
+      exclude: [],
+    },
   },
 });
