5bca25503859a45d7a21f086ddcc8b71fe225e2c
baeni <baeni.saa@gmail.com>
2023-08-29 22:32:09 +0200

    Create userSettingsStore

---
+++ imgrio-nuxt/stores/userSettings.ts
@@ -0,0 +1,21 @@
+﻿import { ref } from 'vue';
+import { defineStore } from 'pinia';
+
+import type { UserSettings } from '@/models';
+import { apiClient } from '@/axios.conf';
+
+export const useUserSettingsStore = defineStore('useUserSettings', () => {
+  const userSettings = ref<UserSettings>();
+
+  async function fetchUserSettings() {
+    const user = useSupabaseUser();
+
+    if (!user) {
+      return new Error('Not authenticated');
+    }
+
+    userSettings.value = (await apiClient.get(`users/settings/${user.value.id}`)).data;
+  }
+
+  return { fetchUserSettings, userSettings };
+});
