1a4749d069a1cefadff2af1b171bbcd303da891b
baeni <baeni.saa@gmail.com>
2023-08-29 17:42:44 +0200

    Move logout.vue into /auth/ directory; Add template & delay

--- imgrio-nuxt/pages/logout.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { onMounted } from "vue";
-import { signOutUser } from "../composables/useSupabase";
-
-onMounted(async () => {
-  await signOutUser();
-  navigateTo("/");
-});
-</script>
