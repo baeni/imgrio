d8acaec08d0c46edbe08e7b423a7fcd3c2a1fb66
baeni <baeni.saa@gmail.com>
2023-08-29 21:03:16 +0200

    Add UserSettings model

---
+++ imgrio-api/Models/UserSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace imgrio_api.Models;
+
+public class UserSettings
+{
+    public UserSettings(
+        Guid userId,
+        bool imageAnimation,
+        string language)
+    {
+        UserId = userId;
+        ImageAnimation = imageAnimation;
+        Language = language;
+    }
+    
+    [Key]
+    public Guid UserId { get; private set; }
+    public bool ImageAnimation { get; set; }
+    public string Language { get; set; }
+}
