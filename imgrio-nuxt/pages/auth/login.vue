439e5ebf509cd7e52654c84c320ef440c9817e00
baeni <baeni.saa@gmail.com>
2023-08-29 17:41:14 +0200

    Simplify import

--- imgrio-nuxt/pages/auth/login.vue
+++ imgrio-nuxt/pages/auth/login.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
-import { signInUser } from "../composables/useSupabase";
+import { signInUser } from "~/composables/useSupabase";
 
 onMounted(() => {
   signInUser();
