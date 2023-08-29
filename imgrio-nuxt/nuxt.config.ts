b948a6b6eda26ec0ad5c24ad91c075bc784d3c1d
baeni <baeni.saa@gmail.com>
2023-08-29 18:40:15 +0200

    Fix formatting

--- imgrio-nuxt/nuxt.config.ts
+++ imgrio-nuxt/nuxt.config.ts
@@ -2,46 +2,46 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  css: ["@/assets/main.css", "@fortawesome/fontawesome-svg-core/styles.css"],
+  css: ['@/assets/main.css', '@fortawesome/fontawesome-svg-core/styles.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {},
-    },
+      autoprefixer: {}
+    }
   },
 
-  modules: ["@pinia/nuxt", "@nuxtjs/i18n", "@nuxtjs/supabase", "@nuxt/image"],
-  build: { transpile: ["vue-toastification"] },
+  modules: ['@pinia/nuxt', '@nuxtjs/i18n', '@nuxtjs/supabase', '@nuxt/image'],
+  build: { transpile: ['vue-toastification'] },
 
   runtimeConfig: {
     public: {
-      siteUrl: process.env.SITE_URL,
-    },
+      siteUrl: process.env.SITE_URL
+    }
   },
 
   app: {
     head: {
-      title: "imgrio",
-    },
+      title: 'imgrio'
+    }
   },
 
   i18n: {
     // https://i18n.nuxtjs.org/options-reference/
-    vueI18n: "./i18n.conf.ts",
+    vueI18n: './i18n.conf.ts',
     detectBrowserLanguage: false, // temporarily disabled
     locales: [
-      { code: "de", iso: "de", file: "de.json" },
-      { code: "en", iso: "en", file: "en.json" },
+      { code: 'de', iso: 'de', file: 'de.json' },
+      { code: 'en', iso: 'en', file: 'en.json' }
     ],
-    langDir: "./locales",
-    strategy: "no_prefix",
+    langDir: './locales',
+    strategy: 'no_prefix'
   },
 
   supabase: {
     redirectOptions: {
-      login: "/auth/login",
-      callback: "/",
-      exclude: ["/", "/sharex", "/v/*"],
-    },
-  },
+      login: '/auth/login',
+      callback: '/',
+      exclude: ['/', '/sharex', '/v/*']
+    }
+  }
 });
