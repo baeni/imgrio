67260a5b22fe03e58cf21b55dd9daa62cf31feb3
baeni <baeni.saa@gmail.com>
2023-08-30 21:03:52 +0200

    Rename and Redefine UserSetting Model

--- imgrio-api/Data/ImgrioDbContext.cs
+++ imgrio-api/Data/ImgrioDbContext.cs
@@ -6,7 +6,7 @@ namespace imgrio_api.Data
     public class ImgrioDbContext : DbContext
     {
         public DbSet<UserFile> UserFiles { get; set; }
-        public DbSet<UserSettings> UserSettings { get; set; }
+        public DbSet<UserSetting> UserSettings { get; set; }
 
         public ImgrioDbContext(DbContextOptions options) : base(options) { }
     }
---
+++ imgrio-api/Models/UserSetting.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace imgrio_api.Models;
+
+public class UserSetting
+{
+    public UserSetting(
+        Guid id,
+        Guid userId,
+        string key,
+        string value)
+    {
+        Id = id;
+        UserId = userId;
+        Key = key;
+        Value = value;
+    }
+    
+    [Key]
+    public Guid Id { get; private set; }
+    public Guid UserId { get; private set; }
+    public string Key { get; private set; }
+    public string Value { get; internal set; }
+}
