36f957ee6655a62845e708c802acc35597423616
baeni <baeni.saa@gmail.com>
2023-08-30 21:04:25 +0200

    Create UserFileInfo model

---
+++ imgrio-api/Models/UserFilesInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace imgrio_api.Models
+{
+    public class UserFilesInfo
+    {
+        public UserFilesInfo(int count, int countToday)
+        {
+            Count = count;
+            CountToday = countToday;
+        }
+
+        public int Count { get; set; }
+        public int CountToday { get; set; }
+    }
+}
