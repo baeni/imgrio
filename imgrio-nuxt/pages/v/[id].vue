7ee9d6def45f9edaf9de7241553d1e1bf53bb2a1
baeni <baeni.saa@gmail.com>
2023-09-20 20:24:39 +0200

    Set min width and height for image; Change border color for title

--- imgrio-nuxt/pages/v/[id].vue
+++ imgrio-nuxt/pages/v/[id].vue
@@ -27,13 +27,13 @@ const userContent = ref<UserContent|null>(null);
     <span class="absolute w-full h-full -z-10 bg-no-repeat bg-cover bg-center" :style="`background-image: url(${userContent.url}); filter: blur(150px);`" v-if="userContent" />
 
     <div>
-      <NuxtImg class="max-w-full max-h-[70vh] rounded-xl" :src="userContent.url" :alt="userContent.title" placeholder v-if="userContent" />
+      <NuxtImg class="max-w-full max-h-[70vh] min-w-[25vw] min-h-[50vh] rounded-xl" :src="userContent.url" :alt="userContent.title" placeholder v-if="userContent" />
       <Skeleton class="h-[50vh] aspect-video rounded-xl" v-else />
       
       <div class="mt-4">
         <div class="w-fit mx-auto">
           <div v-if="userContent">
-            <p class="text-2xl font-semibold px-6 py-4 truncate bg-zinc-950 bg-opacity-20 backdrop-blur-md border border-zinc-50 border-opacity-20 rounded-2xl">{{ userContent.title }}</p>
+            <p class="text-2xl font-semibold px-6 py-4 truncate bg-zinc-950 bg-opacity-20 backdrop-blur-md border border-zinc-400 border-opacity-20 rounded-2xl">{{ userContent.title }}</p>
 
             <AlertDialog>
               <AlertDialogTrigger class="float-right mr-2 opacity-40">
