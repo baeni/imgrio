4a559cf7fb63a49c80d5f701b72b7b35518c3123
baeni <baeni.saa@gmail.com>
2023-09-20 21:02:04 +0200

    Add logic to upload files

--- imgrio-nuxt/pages/dashboard/files.vue
+++ imgrio-nuxt/pages/dashboard/files.vue
@@ -1,6 +1,7 @@
 ﻿<script setup lang="ts">
-import { onMounted, computed } from 'vue';
+import { onMounted, computed, ref } from 'vue';
 import { useUserContentsStore } from '~/stores/userContents';
+import { UserContent } from '~/models'
 import {
   Dialog,
   DialogContent,
@@ -22,6 +23,34 @@ onMounted(async () => {
 });
 
 const userContents = computed(() => userContentsStore.userContents);
+const selectedFile = ref<File | null>(null);
+
+const handleFileChange = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  if (input.files && input.files.length > 0) {
+    selectedFile.value = input.files[0];
+  }
+};
+
+async function handlePostAsync() {
+  try {
+    if (!selectedFile.value) {
+      console.log("No file selected.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', selectedFile.value);
+    const response = await userContentsStore.postUserContentAsync(formData);
+
+    // copyToClipboard(response.url);
+    console.log("Link copied to clipboard.");
+    selectedFile.value = null;
+  } catch {
+    console.log("An error has occured.");
+    return;
+  }
+}
 </script>
 
 <template>
@@ -41,12 +70,12 @@ const userContents = computed(() => userContentsStore.userContents);
               Files with up to 10 MB allowed.
             </DialogDescription>
           </DialogHeader>
-          <div class="p-10 rounded-lg transition-colors border border-dashed border-zinc-400 border-opacity-20 lg:hover:border-opacity-100">
-            <input class="hidden" id="file" type="file" accept="" />
-            <Label class="text-center select-none cursor-pointer" for="file">Drag'n'Drop file here<br>or select</Label>
+          <div class="rounded-lg transition-colors border-2 border-dashed border-zinc-400 border-opacity-50 lg:hover:border-opacity-100">
+            <input class="hidden" id="file" type="file" accept="" @change="handleFileChange" />
+            <Label class="p-10 text-center select-none cursor-pointer" for="file">Drag'n'Drop file here<br>or select</Label>
           </div>
           <DialogFooter>
-            <Button type="submit">
+            <Button @click="async () => await handlePostAsync()">
               Upload
             </Button>
           </DialogFooter>
