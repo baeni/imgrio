993bee94d8515a248d6f7d06dd4c2c74d0fd7aa5
baeni <baeni.saa@gmail.com>
2023-09-21 17:47:06 +0200

    Remove extended-property; Move conditions to app.vue

--- imgrio-nuxt/layouts/default.vue
+++ imgrio-nuxt/layouts/default.vue
@@ -1,16 +1,10 @@
 ﻿<script setup lang="ts">
 import Navbar from '@/components/Navbar.vue'
-
-const props = defineProps({
-  extended: {
-    type: Boolean,
-    default: false
-  }
-});
 </script>
 
 <template>
-  <Navbar :transparent="$route.path.startsWith('/v/') || $route.path == '/'" />
+  <Navbar transparent />
+  
   <main>
     <slot />
   </main>
