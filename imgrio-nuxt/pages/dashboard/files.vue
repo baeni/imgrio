b622a53af297fe4076efc8e68222548866be2cde
baeni <baeni.saa@gmail.com>
2023-09-21 21:11:22 +0200

    Move up const userContents; replace null check with size check of array (as it can no longer be undefined or null)

--- imgrio-nuxt/pages/dashboard/files.vue
+++ imgrio-nuxt/pages/dashboard/files.vue
@@ -4,18 +4,18 @@ import { useUserContentsStore } from '~/stores/userContents';
 import UserContentGrid from "~/components/UserContentGrid.vue";
 
 const userContentsStore = useUserContentsStore();
+const userContents = computed(() => userContentsStore.userContents);
 
 onMounted(async () => {
   await userContentsStore.fetchDataAsync();
 });
 
-const userContents = computed(() => userContentsStore.userContents);
 </script>
 
 <template>
   <section class="container pt-28">
     <p class="text-2xl font-bold">My Files</p>
     
-    <UserContentGrid :userContents="userContents" v-if="userContents" />
+    <UserContentGrid :userContents="userContents" v-if="userContents.length > 0" />
   </section>
 </template>
