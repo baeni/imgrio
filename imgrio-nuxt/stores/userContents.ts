f8e549559f1a058c806f3b28d605130eb4449231
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2023-09-24 17:37:29 +0200

    Save the response of fetchDataAsync request in a const and return that; Remove nullcheck for userContents.value; In postUserContentAsync function get response.userFIle instead of response.userContent (Temporary change as the backend still returns userFIle instead of userContent)

--- imgrio-nuxt/stores/userContents.ts
+++ imgrio-nuxt/stores/userContents.ts
@@ -8,24 +8,26 @@ export const useUserContentsStore = defineStore('userContents', () => {
   const userContents = ref<UserContent[]>([]);
 
   async function fetchDataAsync() {
-    userContents.value = (await apiClient.get('me/files')).data.sort(
-      (a: UserContent, b: UserContent) =>
-        new Date(b.dateOfCreation).getTime() - new Date(a.dateOfCreation).getTime()
+    const response = (await apiClient.get('me/files')).data.sort(
+        (a: UserContent, b: UserContent) =>
+            new Date(b.dateOfCreation).getTime() - new Date(a.dateOfCreation).getTime()
     );
+    userContents.value = response;
+    return response;
   }
 
   async function postUserContentAsync(formData: FormData) {
     const response = (await apiClient.post('me/files', formData)).data;
-    userContents.value?.unshift(response.userContent);
+    userContents.value.unshift((response.userFile));
     return response;
   }
 
-  async function deleteUserContentAsync(userContentId: string) {
-    const response = (await apiClient.delete(`me/files/${userContentId}`)).data;
-    const index = userContents.value?.findIndex(
-      (selectedUserContent) => selectedUserContent.id == userContentId
+  async function deleteUserContentAsync(userContent: UserContent) {
+    const response = (await apiClient.delete(`me/files/${userContent.id}`)).data;
+    const index = userContents.value.findIndex(
+      (selectedUserContent) => selectedUserContent.id == userContent.id
     );
-    userContents.value?.splice(index!, 1);
+    userContents.value.splice(index, 1);
     return response;
   }
 
