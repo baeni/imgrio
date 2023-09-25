757b9435d1358a60eb95b7d53d8790069ebb6db7
baeni <baeni.saa@gmail.com>
2023-09-25 21:03:33 +0200

    Implement const "fetched" to conditionally show loading and other messages

--- imgrio-nuxt/pages/dashboard/files.vue
+++ imgrio-nuxt/pages/dashboard/files.vue
@@ -1,13 +1,16 @@
 ﻿<script setup lang="ts">
 import { onMounted, computed } from 'vue';
 import { useUserContentsStore } from '~/stores/userContents';
-import UserContentGrid from '~/components/UserContentGrid.vue';
+import Loading from '@/components/Loading.vue';
+import UserContentGrid from '@/components/UserContentGrid.vue';
 
+const fetched = ref<boolean>(false);
 const userContentsStore = useUserContentsStore();
 const userContents = computed(() => userContentsStore.userContents);
 
 onMounted(async () => {
   await userContentsStore.fetchDataAsync();
+  fetched.value = true;
 });
 </script>
 
@@ -16,5 +19,9 @@ onMounted(async () => {
     <p class="text-2xl font-bold">My Files</p>
 
     <UserContentGrid :userContents="userContents" v-if="userContents.length > 0" />
+    <Loading class="flex w-full justify-center mt-4" v-else-if="!fetched" />
+    <div class="mt-4" v-else>
+      <p class="text-xl text-zinc-500 font-bold">It seems you did not upload any content — yet</p>
+    </div>
   </section>
 </template>
