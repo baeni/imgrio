2657aafb615d0f295c389fcb641b6bf93dbb2bc0
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2024-02-08 17:46:18 +0100

    - Create PostUserContentResponseDto

---
+++ imgrio-api/Dtos/PostUserContentResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using imgrio_api.Entities;
+
+namespace imgrio_api.Dtos
+{
+    public class PostUserContentResponseDto
+    {
+        public PostUserContentResponseDto(UserContent userContent, string url)
+        {
+            UserContent = userContent;
+            Url = url;
+        }
+
+        public UserContent UserContent { get; init; }
+        public string Url { get; init; }
+    }
+}
